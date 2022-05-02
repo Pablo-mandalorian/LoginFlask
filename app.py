87145344a6744dd6467826fe89cb79c0eedc39b9
Pablo-mandolorian <pablotelles935@gmail.com>
2022-05-01 21:32:06 -0500

    Firts Commit

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
-from crypt import methods
-from unicodedata import name
+import traceback
 from flask import Flask, redirect, render_template, request, session, url_for
 import mysql.connector
 
-conn = mysql.connector.connect(host="bdjynruf6ziskqxualjp-mysql.services.clever-cloud.com", port="3306", user="", password="", database="bdjynruf6ziskqxualjp")
+conn = mysql.connector.connect(host="localhost", port="3306", user="root", password="root", database="ayuntamiento")
 cursor= conn.cursor()
 
 app = Flask(__name__)
@@ -23,7 +22,7 @@ def login():
     if request.method=='POST':
         username=request.form['username']
         password=request.form['password']
-        cursor.execute('SELECT * from LoginUsers where User=%s and Password=%s', (username,password))
+        cursor.execute('SELECT * from usuario where idusuario=%s and password=%s', (username,password))
         record = cursor.fetchone()
         if record:
             session['loggedin']=True
@@ -36,9 +35,28 @@ def login():
 
 @app.route('/cerrar')
 def cerrar():
-    session.pop('leggedin',None)
+    session.pop('loggedin',None)
     session.pop('username',None)
     return redirect(url_for('login'))
+
+@app.route('/regist')
+def regist():
+    return render_template('new_user.html')
+
+@app.route('/signin', methods=['GET','POST'])
+def signin():
+    user = request.args.get('username')
+    id_user = request.args.get('id_user')
+    password = request.args.get('password')
+    id_privilegios = 3
+    if  id_user   and  user   and  password:
+        cursor.execute('INSERT INTO `usuario` (`idusuario`, `user`, `password`, `idprivilegios`) VALUES (%s, %s, %s, %s)',(id_user,user,password,id_privilegios))
+        conn.commit()
+        return redirect(url_for('login'))
+    else:
+        traceback.print_exc()
+        #return render_template('handling/error/error-saving-data.html')
+    
 
 if __name__ == "__main__":
     app.run(debug=True) 
